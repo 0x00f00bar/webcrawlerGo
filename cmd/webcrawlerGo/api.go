5329463b9e519b9a992a7f609d0ac0d793c2945d
Upendra Meena <0x00f00bar@gmail.com>
2025-01-23 23:41:47 +0530

    added url handlers

--- cmd/webcrawlerGo/api.go
+++ cmd/webcrawlerGo/api.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/0x00f00bar/webcrawlerGo/models"
@@ -44,6 +40,11 @@ func (app *webapp) serve(ctx context.Context, quitChan chan os.Signal) error {
 
 	app.Logger.Printf("starting server on %s", srv.Addr)
 
+	go func() {
+		time.Sleep(time.Second)
+		app.openBrowser(fmt.Sprintf("http://localhost:%d", serverPort))
+	}()
+
 	err := srv.ListenAndServe()
 	if !errors.Is(err, http.ErrServerClosed) {
 		return err
@@ -63,99 +64,10 @@ func (app *webapp) routes() http.Handler {
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
+	router.HandlerFunc(http.MethodGet, "/v1/url", app.listURLHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/url/:id", app.getURLByIdHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/url", app.createURLHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/url/:id", app.updateURLHandler)
+
 	return router
-}
-
-func (app *webapp) readIDParam(r *http.Request) (int64, error) {
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil {
-		return 0, errors.New("invalid id parameter")
-	}
-
-	return id, nil
-}
-
-func (app *webapp) writeJSON(
-	w http.ResponseWriter,
-	status int,
-	data envelope,
-	headers http.Header,
-) error {
-	js, err := json.Marshal(data)
-	if err != nil {
-		return err
-	}
-
-	js = append(js, '\n')
-	for k, v := range headers {
-		w.Header()[k] = v
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	w.Write(js)
-	return nil
-}
-
-func (app *webapp) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
-	maxBytes := readMaxRequestBytes
-	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
-
-	jsDecoder := json.NewDecoder(r.Body)
-	jsDecoder.DisallowUnknownFields()
-
-	err := jsDecoder.Decode(dst)
-	if err != nil {
-		var syntaxError *json.SyntaxError
-		var unmarshalTypeError *json.UnmarshalTypeError
-		var invalidUnmarshalError *json.InvalidUnmarshalError
-		var maxBytesError *http.MaxBytesError
-
-		switch {
-		case errors.As(err, &syntaxError):
-			return fmt.Errorf(
-				"body contains badly-formed JSON (at character %d)",
-				syntaxError.Offset,
-			)
-
-		case errors.Is(err, io.ErrUnexpectedEOF):
-			return errors.New("body contains badly-formed JSON")
-
-		case errors.As(err, &unmarshalTypeError):
-			if unmarshalTypeError.Field != "" {
-				return fmt.Errorf(
-					"body contains incorrect JSON type for field %q",
-					unmarshalTypeError.Field,
-				)
-			}
-			return fmt.Errorf(
-				"body contains incorrect JSON type (at character %d)",
-				unmarshalTypeError.Offset,
-			)
-
-		case errors.Is(err, io.EOF):
-			return errors.New("body must not be empty")
-
-		case strings.HasPrefix(err.Error(), "json: unknown field "):
-			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
-			return fmt.Errorf("body contains unknown key %s", fieldName)
-
-		case errors.As(err, &maxBytesError):
-			return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
-
-		case errors.As(err, &invalidUnmarshalError):
-			panic(err)
-
-		default:
-			return err
-		}
-	}
-
-	err = jsDecoder.Decode(&struct{}{})
-	if err != io.EOF {
-		return errors.New("body must only contain a single JSON value")
-	}
-	return nil
 }
---
+++ cmd/webcrawlerGo/url.go
@@ -0,0 +1,187 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/0x00f00bar/webcrawlerGo/internal"
+	"github.com/0x00f00bar/webcrawlerGo/models"
+)
+
+func (app *webapp) createURLHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		URL string `json:"url"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	var t time.Time
+	url := models.NewURL(input.URL, t, t, true)
+
+	v := internal.NewValidator()
+
+	if models.ValidateURL(v, url); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.Models.URLs.Insert(url)
+	if err != nil {
+		if strings.Contains(err.Error(), "UNIQUE constraint failed") {
+			app.errorResponse(
+				w,
+				r,
+				http.StatusConflict,
+				fmt.Sprintf("url '%s' is already present", url.URL),
+			)
+			return
+		}
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/url/%d", url.ID))
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"url": url}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *webapp) getURLByIdHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	url, err := app.Models.URLs.GetById(int(id))
+	if err != nil {
+		switch {
+		case errors.Is(err, models.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"url": url}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *webapp) updateURLHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	// pointer type to identify if fields present in JSON
+	// json items with null values will be ignored and
+	// will remain unchanged
+	var input struct {
+		IsMonitored *bool `json:"is_monitored"`
+	}
+
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	url, err := app.Models.URLs.GetById(int(id))
+	if err != nil {
+		switch {
+		case errors.Is(err, models.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	if input.IsMonitored != nil {
+		url.IsMonitored = *input.IsMonitored
+	}
+
+	v := internal.NewValidator()
+
+	if models.ValidateURL(v, url); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.Models.URLs.Update(url)
+	if err != nil {
+		switch {
+		case errors.Is(err, models.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"url": url}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *webapp) listURLHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		models.URLFilter
+		models.CommonFilters
+	}
+
+	v := internal.NewValidator()
+	qs := r.URL.Query()
+
+	input.URLFilter.URL = app.readString(qs, "url", "")
+	input.IsMonitored, input.IsMonitoredPresent = app.readBool(qs, "is_monitored", v)
+	input.IsAlive, input.IsAlivePresent = app.readBool(qs, "is_alive", v)
+
+	input.CommonFilters.Page = app.readInt(qs, "page", 1, v)
+	input.CommonFilters.PageSize = app.readInt(qs, "page_size", 10, v)
+	input.CommonFilters.Sort = app.readString(qs, "sort", "id")
+	var safeSortList []string
+	safeSortList = append(safeSortList, models.URLColumns...)
+	safeSortList = append(safeSortList, internal.PrefixString(models.URLColumns, "-")...)
+	input.CommonFilters.SortSafeList = safeSortList
+
+	if models.ValidateCommonFilters(v, input.CommonFilters); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	urls, err := app.Models.URLs.GetAll(input.URLFilter, input.CommonFilters)
+	if err != nil {
+		switch {
+		case errors.Is(err, models.ErrInvalidOrderBy):
+			app.badRequestResponse(w, r, err)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	if urls == nil {
+		urls = []*models.URL{}
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"url_list": urls}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
