d49896c261a134ae1100b96d23e3ec4d12b30bdb
Upendra Meena <0x00f00bar@gmail.com>
2025-02-14 15:24:43 +0530

    added metadata type of paginated queries

--- models/filters.go
+++ models/filters.go
@@ -1,6 +1,10 @@
 package models
 
-import "github.com/0x00f00bar/webcrawlerGo/internal"
+import (
+	"math"
+
+	"github.com/0x00f00bar/webcrawlerGo/internal"
+)
 
 type CommonFilters struct {
 	Page         int
@@ -24,4 +28,26 @@ func ValidateCommonFilters(v *internal.Validator, f CommonFilters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	v.Check(internal.PermittedValue(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
+}
+
+type Metadata struct {
+	CurrentPage  int `json:"current_page,omitempty"`
+	PageSize     int `json:"page_size,omitempty"`
+	FirstPage    int `json:"first_page,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+		TotalRecords: totalRecords,
+	}
 }
